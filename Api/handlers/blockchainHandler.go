1ee9f93fbd18f85f1b3b9a133413a8f2fb33c717
liensanchez <liensilviosanchez@gmail.com>
2023-05-05 15:29:36 -0300

    get blockcain

--- Api/handlers/blockchainHandler.go
+++ Api/handlers/blockchainHandler.go
@@ -5,9 +5,11 @@ import "github.com/gofiber/fiber/v2"
 /* Get test */
 
 func GetBlockchain(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Esto es get Blockchain",
-	})
+	response, err := services.GetBlockchain()
+	if err != nil {
+		return c.SendString("Error in get")
+	}
+	return c.JSON(response)
 }
 
 func PostBlockchain(c *fiber.Ctx) error {
